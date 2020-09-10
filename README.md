a8be74d73b13ba2a08bec36cd1fc93913a42e102
Andrei Ciortea <andrei.ciortea@gmail.com>
2020-09-10 19:15:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# RDFSub Hub
+
+This project is a Java implementation of an RDFSub Hub based on [Corese](https://github.com/Wimmics/corese).
+
+## Prerequisites
+
+* JDK 8+
+
+## Running the project
+
+    ./gradlew test run
+
+The command compiles the project and runs the tests, then it launches the application, so you can check by yourself. Open your browser to http://localhost:8080. You should see a _Hello World_ message.
+
+## Building the project
+
+To build the project, just use:
+
+    ./gradlew build
+
+It generates a _fat-jar_ in the `build/libs` directory.
---
+++ src/test/java/org/hyperagents/rdfsub/MainVerticleTest.java
@@ -0,0 +1,39 @@
+package org.hyperagents.rdfsub;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.hyperagents.rdfsub.MainVerticle;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+
+@ExtendWith(VertxExtension.class)
+class MainVerticleTest {
+
+  @BeforeEach
+  void prepare(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(MainVerticle.class.getCanonicalName(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  @DisplayName("Check that the server has started")
+  void checkServerHasStarted(Vertx vertx, VertxTestContext testContext) {
+    WebClient webClient = WebClient.create(vertx);
+    webClient.get(8080, "localhost", "/")
+      .as(BodyCodec.string())
+      .send(testContext.succeeding(response -> testContext.verify(() -> {
+        assertEquals(200, response.statusCode());
+        assertTrue(response.body().length() > 0);
+        assertTrue(response.body().contains("Hello Vert.x!"));
+        testContext.completeNow();
+      })));
+  }
+}
